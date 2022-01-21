1964ff4083d2a83119804705468f5237ea4a42bb
rajyeola <rajsyeola@gmail.com>
2022-01-21 17:40:10 +0530

    updated API url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = "https://footlearn-api.herokuapp.com/";
 
 export const signUp = (userInfo) => axios.post(`${url}/user/signup`, userInfo);
 export const signIn = (userInfo) => axios.post(`${url}/user/signin`, userInfo);
